36c3077f22e7e5e3f1ea2bf62cbfdc4ee3ef2e43
lebinbon <lengocbutchi@gmail.com>
2026-03-12 15:16:23 +0700

    update_price.js

--- update_price.js
+++ update_price.js
@@ -1,49 +1,37 @@
-const https = require("https");
+const axios = require("axios");
+const cheerio = require("cheerio");
 const fs = require("fs");
 
-const PRICE_FILE = "price.json";
+async function getPrice() {
 
-function getHTML() {
-  return new Promise((resolve, reject) => {
+const url = "https://www.petrolimex.com.vn";
 
-    https.get("https://www.petrolimex.com.vn", res => {
+const { data } = await axios.get(url);
 
-      let data = "";
+const text = data.replace(/\s+/g," ");
 
-      res.on("data", chunk => data += chunk);
+function find(keyword){
 
-      res.on("end", () => resolve(data));
+const r = new RegExp(keyword + ".*?(\\d{2}\\.\\d{3})");
+const m = text.match(r);
 
-    }).on("error", reject);
+return m ? m[1] : "00.000";
 
-  });
 }
 
-function findPrice(html, keyword) {
+const price = {
 
-  const regex = new RegExp(keyword + "[^0-9]*(\\d{2}\\.\\d{3})");
+ron95: find("RON 95"),
+e5: find("E5"),
+do001: find("0,001"),
+do005: find("0,05")
 
-  const match = html.match(regex);
+};
 
-  return match ? match[1] : "00.000";
+fs.writeFileSync("price.json", JSON.stringify(price,null,2));
+
+console.log(price);
+
 }
 
-async function run() {
-
-  const html = await getHTML();
-
-  const price = {
-
-    ron95: findPrice(html, "RON 95"),
-    e5: findPrice(html, "E5"),
-    do001: findPrice(html, "0,001"),
-    do005: findPrice(html, "0,05")
-
-  };
-
-  fs.writeFileSync(PRICE_FILE, JSON.stringify(price, null, 2));
-
-  console.log("Updated:", price);
-}
-
-run();
+getPrice();
