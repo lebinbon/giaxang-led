6bfe741c8a8c9c164a1a138f207ff74534f48c21
lebinbon <lengocbutchi@gmail.com>
2026-03-12 13:49:09 +0700

    update_price.js

--- update_price.js
+++ update_price.js
@@ -1,55 +1,84 @@
 const fs = require("fs");
 
-async function update() {
+async function updatePrice() {
 
-    const res = await fetch("https://www.petrolimex.com.vn");
-
+    const res = await fetch("https://www.petrolimex.com.vn/");
     const html = await res.text();
 
-    function find(keyword) {
+    function getPrice(name) {
 
-        const regex = new RegExp(keyword + "[^0-9]*([0-9]{2}\\.[0-9]{3})");
+        const regex = new RegExp(name + "[^0-9]{0,50}([0-9]{2}\\.[0-9]{3})","i");
+        const match = html.match(regex);
 
-        const m = html.match(regex);
+        if(match){
+            return match[1];
+        }
 
-        return m ? m[1] : "00.000";
+        return "00.000";
     }
 
-    const data = {
-        ron95: find("RON 95"),
-        e5: find("E5 RON 92"),
-        do001: find("0,001"),
-        do05: find("0,05")
-    };
+    const ron95 = getPrice("RON 95");
+    const e5 = getPrice("E5 RON 92");
+    const do001 = getPrice("0,001");
+    const do05 = getPrice("0,05");
 
-    const html_out = `
+    console.log("RON95:",ron95);
+    console.log("E5:",e5);
+    console.log("DO001:",do001);
+    console.log("DO05:",do05);
+
+    const html_output = `
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
 <style>
-body{margin:0;background:black;color:#FFD700;font-family:Arial;font-size:28px;font-weight:bold}
-.container{width:1872px;height:82px;display:flex;align-items:center;justify-content:space-around}
+body{
+margin:0;
+background:black;
+color:#FFD700;
+font-family:Arial;
+font-size:28px;
+font-weight:bold;
+}
+
+.container{
+width:1872px;
+height:82px;
+display:flex;
+align-items:center;
+justify-content:space-around;
+}
+
 .white{color:white}
 .green{color:#00FF00}
+
 </style>
 </head>
+
 <body>
+
 <div class="container">
 
 <span class="white">GIÁ XĂNG PETROLIMEX</span>
-<span><span class="white">RON95:</span> ${data.ron95}</span>
-<span><span class="white">E5:</span> ${data.e5}</span>
-<span><span class="white">DO 0.001:</span> <span class="green">${data.do001}</span></span>
-<span><span class="white">DO 0.05:</span> <span class="green">${data.do05}</span></span>
+
+<span><span class="white">RON95:</span> ${ron95}</span>
+
+<span><span class="white">E5:</span> ${e5}</span>
+
+<span><span class="white">DO 0.001:</span> <span class="green">${do001}</span></span>
+
+<span><span class="white">DO 0.05:</span> <span class="green">${do05}</span></span>
 
 </div>
+
 </body>
 </html>
 `;
 
-    fs.writeFileSync("giaxang_v1.html", html_out);
-    fs.writeFileSync("giaxang_v2.html", html_out);
+    fs.writeFileSync("giaxang_v1.html", html_output);
+    fs.writeFileSync("giaxang_v2.html", html_output);
+
 }
 
-update();
+updatePrice();
